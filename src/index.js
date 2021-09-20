b7f51d0d899f7cbb6c55ae20a403189fc7693515
ederdaniel22 <eder.infonet@gmail.com>
2021-09-20 10:49:41 -0400

    Alterando para Nodemon

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/courses', (request, response)=>{
+    const query = request.query;
+    console.log(query);
     return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
@@ -11,6 +13,8 @@ app.post("/courses", (request, response)=>{
 })
 
 app.put("/courses/:id", (request, response)=>{
+    const { id } = request.params;
+    console.log( id );
     return response.json(["Curso 6", "Curso 2", "Curso 3", "curso 4"]);
 })
 
