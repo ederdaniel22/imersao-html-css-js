ab0f61a8fd006cd0fe874dbce309def92ce25620
ederdaniel22 <eder.infonet@gmail.com>
2021-09-22 19:03:01 -0400

    Finalizando metodos e utilizando insomnia

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/courses', (request, response)=>{
     const query = request.query;
     console.log(query);
@@ -9,6 +11,8 @@ app.get('/courses', (request, response)=>{
 });
 
 app.post("/courses", (request, response)=>{
+    const body = request.body;
+    console.log(body);
     return response.json(["Curso 1", "Curso 2", "Curso 3", "curso 4"]);
 })
 
